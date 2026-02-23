99ba24e7fa2b2bd5092032ed00487a7ab536311f
tao2825 <hdhhsdjdj@gmail.com>
2026-02-23 11:49:06 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,6 +4,13 @@ import controller
 
 def render_login():
     st.title("🔐 เข้าสู่ระบบ")
+
+st.subheader("👤 ข้อมูลผู้จัดทำ")
+    st.write("**ชื่อ: นายวีรพงษ์ แวววงศ์")
+    st.write("**รหัสนักศึกษา:** 6740259113")
+    st.write("**หมู่เรียน:** ว.6706")
+    st.divider() # เส้นคั่นเพื่อความเรียบร้อยเหมือนในรูป
+    
     with st.form("login_form"):
         username = st.text_input("ชื่อผู้ใช้", placeholder="เช่น admin")
         password = st.text_input("รหัสผ่าน", type="password", placeholder="เช่น 1234")
@@ -22,4 +29,5 @@ def render_login():
                 st.session_state["is_logged_in"] = True
                 st.session_state["user"] = user_info
                 st.session_state["page"] = "books" # หรือให้ไป borrows ก็ได้
+
                 st.rerun()
